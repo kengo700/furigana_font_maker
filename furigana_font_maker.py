db69e1a8ec33c6318f5dd593bb0750089451ef3d
kengo700 <kengo700blog@gmail.com>
2019-01-23 22:06:04 +0900

    まずはフォントを開いて別名で保存するだけの処理
    下記ページを参考に
    http://gihyo.jp/admin/serial/01/ubuntu-recipe/0332?page=2
    
    Times New Romanは保存できたが、Noto Sans CJKだと上手くいかない（Font Forgeで開くと、大半の文字が表示されない）

---
+++ furigana_font_maker.py
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env python2
+# -*- coding: utf-8; -*-
+
+import sys
+import fontforge
+
+if __name__ == '__main__':
+    src = sys.argv[1]
+    dst = sys.argv[2]
+    font = fontforge.open(src)
+    font.generate(dst)
+    font.close()
