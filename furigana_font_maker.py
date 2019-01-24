b4ae217bcfd92d69b7e2131d3a0a894446e3b63e
kengo700 <kengo700blog@gmail.com>
2019-01-24 21:27:08 +0900

    「ABC」の文字の中央に「A」のルビを振るように位置を調整。試すためにフォント名を編集
    フォント名を変えているのは、フォントをインストールする時にオリジナルのフォントを上書きしないように
    
    生成したフォントをインストールし、Wordで試して「ABC」の文字の中央に「A」のルビが振れることを確認
    Wordで「U+E000」の入力する方法は下記
    https://techracho.bpsinc.jp/baba/2013_04_19/8113

--- furigana_font_maker.py
+++ furigana_font_maker.py
@@ -11,30 +11,44 @@ if __name__ == '__main__':
     dst = sys.argv[2]
     font = fontforge.open(src)
 
+    furigana_scale = 0.5
+
+    print font.fontname
+    print font.fullname
+
+    font.fontname = font.fontname + "_furigana"
+    font.fullname = font.fullname + " Furigana"
+    font.familyname = font.familyname + " Furigana"
+
     oyamoji_all = "ABC"
     oyamoji_list = list(oyamoji_all)
 
     oyamoji_width = 0
     for oyamoji in oyamoji_list:
         oyamoji_width += font[oyamoji].width
-        print font[oyamoji].width
 
-    print oyamoji_width
-    print oyamoji_width / 2
+    oyamoji_center = oyamoji_width / 2
 
-    font.selection.select("A")
+    furigana_all = "A"
+    furigana_list = list(furigana_all)
+
+    font.selection.select(furigana_list[0])
     font.copy()
 
     font.selection.select(('more', 'unicode'), 0xE000)
     font.paste()
 
+    furigana_width = font[furigana_list[0]].width;
+
+    furigana_x = -oyamoji_center - furigana_width/2*furigana_scale
+
     ascent = font.ascent
 
-    matrix = psMat.compose(psMat.scale(0.5), psMat.translate(0, ascent))
+    matrix = psMat.compose(psMat.scale(furigana_scale), psMat.translate(furigana_x, ascent))
     font[0xE000].transform(matrix)
 
-    matrix = psMat.compose(psMat.scale(0.5), psMat.translate(1000, ascent))
-    font[0xE000].addReference("B", matrix)
+    #matrix = psMat.compose(psMat.scale(furigana_scale), psMat.translate(1000, ascent))
+    #font[0xE000].addReference("B", matrix)
 
     font[0xE000].width = 0;
 
